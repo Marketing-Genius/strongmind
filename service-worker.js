92facfd7349d8007e62078a22fecb75657cd4c6d
Adam <adam.strongmind@gmail.com>
2025-10-29 10:31:29 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,44 @@
 // service-worker.js
-self.addEventListener('install', e => {
-  console.log('[ServiceWorker] Installed');
+const CACHE_NAME = "strongmind-v2"; // bump this when changes are made
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/app.js",
+  "/assets/spark-tokens-white.png",
+  // include all other static assets
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+// Activate (delete old caches)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });

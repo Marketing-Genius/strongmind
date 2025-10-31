2c6f59806e1b1675fef046ef91fc006a7d59dd83
Adam <adam.strongmind@gmail.com>
2025-10-31 11:36:03 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = "strongmind-v2.011"; // bump this when changes are made
+const CACHE_NAME = "strongmind-v2.012"; // bump this when changes are made
 const urlsToCache = [
   "/",
   "/index.html",
